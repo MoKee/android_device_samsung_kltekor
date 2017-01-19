166099577703bee4d069afb48be9fb5f3b74ea2b
martincz <martincz@mokeedev.com>
2017-01-19 14:51:30 +0800

    kltekor: mokee-ify

--- lineage.mk
+++
@@ -1,6 +0,0 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-$(call inherit-product, device/samsung/kltekor/full_kltekor.mk)
-
-PRODUCT_DEVICE := kltekor
-PRODUCT_NAME := lineage_kltekor
---
+++ mokee.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/kltekor/full_kltekor.mk)
+
+PRODUCT_DEVICE := kltekor
+PRODUCT_NAME := mk_kltekor
